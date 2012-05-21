6188a7b7f48cd1704fb73618e90876249616adb9
Wayne Warner <wawjr3d@gmail.com>
2012-05-20 22:00:34 -0400

    make url-able

--- js/practice/smileys.js
+++ js/practice/smileys.js
@@ -18,7 +18,13 @@
     	// keys i care about
 		KEYUP = 38,
 		KEYDOWN = 40,
-		ENTER_KEY = 13;
+		ENTER_KEY = 13,
+		
+		FIELDS = {
+			SIZE: "size",
+			MOOD: "mood",
+			SPECIFIC: "specific"
+		};
 	
 	
 	var $form, $smiley, $size;
@@ -29,12 +35,17 @@
 		$smiley.css("fontSize", size + "px");
 
 		$smiley.removeClass(ALL_SMILEYS);
-		$smiley.addClass($form.find("input[name=mood]:checked").val());
-		$smiley.addClass($form.find("input[name=specific]:checked").val());
+		$smiley.addClass($form.find("input[name=" + FIELDS.MOOD + "]:checked").val());
+		$smiley.addClass($form.find("input[name=" + FIELDS.SPECIFIC + "]:checked").val());
 	}
 	
 	function killSubmit(e) {
 		e.preventDefault();
+	}
+	
+	function capSize(size) { // not functioning pun
+		if (isNaN(size)) { return MIN_SIZE; }
+		return Math.max(Math.min(size, MAX_SIZE), MIN_SIZE);
 	}
 	
 	function upAndDown(e) {
@@ -42,12 +53,12 @@
     	
         switch (e.keyCode) {
 	        case KEYUP:
-	        	$size.val(Math.min(size + 1, MAX_SIZE));
+	        	$size.val(capSize(size + 1));
 	        	pickSmiley();
 	            break;
 	            
 	        case KEYDOWN:
-	        	$size.val(Math.max(size - 1, MIN_SIZE));
+	        	$size.val(capSize(size - 1));
 	        	pickSmiley();
 	            break;
 	            
@@ -56,18 +67,53 @@
         }
 	}
 	
+	var getQueryStringParam = (function() {
+		
+			var query_string = (window.location.search.length > 1) ? window.location.search.substring(1) : null;
+			var pairs = (query_string != null) ? query_string.split("&") : [];
+			var params = {};
+			
+			for (var i=0; i < pairs.length; i++) {
+				var pair = pairs[i].split("=");
+				
+				params[pair[0]] = pair[1] != null ? pair[1] : null;
+			}
+			
+			return function(name) { return params[name] != null ? decodeURIComponent(params[name]) : null; }
+	})();
+	
+	function inititializeSmiley() {
+		var size = getQueryStringParam(FIELDS.SIZE),
+			mood = getQueryStringParam(FIELDS.MOOD),
+			specific = getQueryStringParam(FIELDS.SPECIFIC);
+		
+		if (size) {
+			$size.val(capSize(parseInt(size || 0, 10)));
+		}
+		
+		if (mood) {
+			$form.find("input[name=" + FIELDS.MOOD + "][value=" + mood + "]").prop("checked", true);
+		}
+		
+		if (specific) {
+			$form.find("input[name=" + FIELDS.SPECIFIC + "][value=" + specific + "]").prop("checked", true);
+		}
+		
+		pickSmiley();
+	}
+	
 	$(function() {
 		$form = $("form.pick-a-smiley");
-		$size = $form.find("input[name=size]");
+		$size = $form.find("input[name=" + FIELDS.SIZE + "]");
 		$smiley = $form.find(".smiley");
 		
 		$form.submit(killSubmit);
 		$form.keydown(upAndDown);
 		
-		$form.find("input[name=mood], input[name=specific]").change(pickSmiley);
+		$form.find("input[name=" + FIELDS.MOOD + "], input[name=" + FIELDS.SPECIFIC + "]").change(pickSmiley);
 		$size.change(pickSmiley)
 		
-		pickSmiley();
+		inititializeSmiley();
 	});
 	
 })(this, jQuery);
